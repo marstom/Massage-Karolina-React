0a000eea206e9e098b3104b3f11132b23caf3707
Tomasz Marszalek <marstomek@gmail.com>
2023-06-30 20:01:13 +0200

    dodanie storybooka

---
+++ src/stories/KarolinaComponents.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {Sekcja} from 'ui/atoms/atomy'
+
+const meta = {
+  title: 'Karolina/Komponenty',
+  // component: Header,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+      //   More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+      layout: 'fullscreen',
+  }
+}
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const My = () => {
+  return (
+      <>
+          <Sekcja>Hejko </Sekcja>
+      </>
+  )
+}
