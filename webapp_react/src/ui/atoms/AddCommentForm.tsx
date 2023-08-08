1a85a16a511f333279c0a8b1263cfeccf5f1c087
Tomasz Marszalek <marstomek@gmail.com>
2023-08-08 16:47:41 +0200

    WIP komentarze mutacja

--- webapp_react/src/ui/atoms/AddCommentForm.tsx
+++ webapp_react/src/ui/atoms/AddCommentForm.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import styled from "styled-components";
 import { colors, otherColors } from "ui/palette";
+import { gql, useMutation } from "@apollo/client";
+import { useParams } from "react-router-dom";
 
 const FormContainer = styled.div`
   display: flex;
@@ -46,19 +48,74 @@ const LabelName = styled(Label)`
   margin-left: auto; // it align whole thing to left
 `;
 
-const AddCommentForm = () => {
-  const addComment = () => {
+type FormData = {
+  comment: string;
+  author: string;
+};
+
+const initialFormData: FormData = {
+  comment: "",
+  author: "",
+};
+
+// TODO move this query to BlogCommentSection level.
+const query = gql`
+  mutation CreatePostComment($post: ID!, $comment: String!, $author: String!) {
+    createPostComment(
+      data: { post: $post, comment: $comment, author: $author }
+    ) {
+      data {
+        id
+      }
+    }
+  }
+`;
+
+const AddCommentForm: React.FC<{}> = () => {
+  const { id } = useParams();
+  const [mutateFunction, { data, loading, error }] = useMutation<
+    { id: string } | FormData
+  >(query);
+  const [formData, setFormData] = useState<FormData>(initialFormData);
+  // useEffect(() => {}, [loading]); // TODO reload all coments
+  const addComment = async () => {
     console.log("COmm added");
+    const mutationVariables = { post: id, ...formData };
+    console.log("Mu varialbesl");
+    console.log(mutationVariables);
+    await mutateFunction({ variables: mutationVariables });
   };
+
+  const handleNameChange = (e: FormEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    formData.author = value;
+    console.log(formData);
+  };
+
+  const handleCommentChange = (e: FormEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.currentTarget;
+    formData.comment = value;
+    console.log(formData);
+  };
+  if (error) {
+    return <>Error!!!</>;
+  }
+  if (loading) {
+    return <>Loading</>;
+  }
   return (
     <>
       <FormContainer>
         <Label>Komentarz: </Label>
-        <TextArea />
+        <TextArea onChange={handleCommentChange} name={"comment"} />
       </FormContainer>
       <ButtonContainer>
         <LabelName>Imię: </LabelName>
-        <NameInput type={"text"}></NameInput>
+        <NameInput
+          name={"name"}
+          type={"text"}
+          onChange={handleNameChange}
+        ></NameInput>
         <Button onClick={() => addComment()}>Dodaj komentarz</Button>
       </ButtonContainer>
     </>
