6e2267faef4d96a3ca6569a2da20963319864418
Tomasz Marszalek <marstomek@gmail.com>
2023-08-26 07:04:13 +0200

    Better dropdowns look

--- webapp_react/src/ui/atoms/DropdownMenu.tsx
+++ webapp_react/src/ui/atoms/DropdownMenu.tsx
@@ -3,29 +3,38 @@ import { NonStyledDropdownEl } from "./NonStyledLink";
 import styled from "styled-components";
 import { colors } from "../palette";
 import { NonStyledLink } from "../atoms/NonStyledLink";
+import "./DropdownMenu.css";
 
 type Props = {
   children: ReactNode;
 };
 
 const Window = styled.div`
-  width: 12%;
-  position: absolute;
+  width: 15%;
+  margin-left: 25px;
+  margin-top: 20px;
+  position: fixed;
   display: flex;
   align-self: flex-start;
   align-items: center;
   color: ${colors.menuTextColor};
 
   background: ${colors.black};
+  border-top: 1mm ${colors.green};
+  border-style: solid;
   box-shadow: 0px 10px 30px 0px ${colors.primaryOpaque};
+  border-radius: 2%;
+  animation: fadeInOut 400ms ease-in-out;
+  transform-origin: top center;
 `;
 
 export const DropdownEl = styled(NonStyledDropdownEl)`
   font-size: 150%;
   margin: 10px;
   padding-block: 15px;
-  padding: 0px 30px 0px 30px;
+  padding: 0px 30px 50px 30px;
   color: ${colors.menuTextColor};
+  //padding-bottom: 50px;
 
   // arrow down
   &::after {
@@ -81,6 +90,7 @@ export const DropdownMenu: React.FC<Props> = ({ children }) => {
     >
       {<DropdownEl>{children}</DropdownEl>}
       {isDropdownVisible && visibleDropdown()}
+      {/*{visibleDropdown()}*/}
     </Span>
   );
 };
